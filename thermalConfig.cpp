b03dd09f92b7b933f1f466bb89291db3271ebb05
Bhanuprakash Reddy Modem <quic_bmodem@quicinc.com>
2025-03-28 19:10:56 +0530

    thermal-hal: Add thermal HAL support for Sun IoT
    Add thermal HAL support for Sun QCS.
    
    Change-Id: I6bd795d98541690ea0e494decb403fe6aa0c3850
    Signed-off-by: Bhanuprakash Reddy Modem <bmodem@qti.qualcomm.com>

--- thermalConfig.cpp
+++ thermalConfig.cpp
@@ -27,15 +27,10 @@
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * Changes from Qualcomm Innovation Center are provided under the following license:
- * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
-
- /* Changes from Qualcomm Innovation Center are provided under the following license:
-
-Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
-SPDX-License-Identifier: BSD-3-Clause-Clear */
 
 #include <unordered_map>
 #include <android-base/logging.h>
