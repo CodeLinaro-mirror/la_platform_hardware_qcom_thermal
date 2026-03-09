0dcb8fe48548b0f35213b7b14e7b09da3b1f9908
Arathi Meenakshy P S <ameenaks@qti.qualcomm.com>
2026-03-10 00:59:17 +0530

    Add SoC ID support or Ravelin IoT variants
    Add SoC ID support or Ravelin IoT variants.
    
    Change-Id: Iceed4bd67b224607fab577ff2b42c20d3707f5e2

--- thermalConfig.cpp
+++ thermalConfig.cpp
@@ -2566,6 +2566,8 @@ namespace thermal {
 		{582, ravelin_common}, //Clarence IOT without modem
 		{653, ravelin_common}, //Clarence Gaming
 		{654, ravelin_common}, //Clarence Gaming
+		{774, ravelin_common}, //Clarence Industrial IOT
+                {773, ravelin_common}, //Clarence Industrial IOT without modem
 		{681, tuna_common}, //Bonito
 		{655, tuna_common}, //Bonito
 		{694, tuna_common}, //Bonito
