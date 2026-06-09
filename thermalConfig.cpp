2dcc4a4dac0c36cc12ec8df4a5ffd231393552a8
Ajay Kumar Reddy Kathi <akathi@qti.qualcomm.com>
2026-06-09 14:59:19 +0530

    thermal-hal: Update skin thermal zone mapping for shikra
    Update skin thermal zone mapping for shikra.
    
    Change-Id: I2fb266ac174c3d9b2f39577e5a4f32794f6dcfe0
    Signed-off-by: Ajay Kumar Reddy Kathi <akathi@qti.qualcomm.com>

--- thermalConfig.cpp
+++ thermalConfig.cpp
@@ -3335,7 +3335,7 @@ namespace thermal {
 	std::vector<struct target_therm_cfg>  shikra_specific = {
 		{
 			TemperatureType::SKIN,
-			{ "sys-therm-0" },
+			{ "sys-therm-2" },
 			"skin",
 			{
 			[LIGHT] = 48000,
